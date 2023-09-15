8a4ef5184340692e590b50fb91c970369cbcaaf8
elissaeng <elissaeng@gmail.com>
2023-09-14 17:33:21 -0700

    lab 1

--- labs/lab-1/pop_growth.c
+++ labs/lab-1/pop_growth.c
@@ -1,46 +1,9 @@
 #include "cs50.h"
 #include <stdio.h>
 
-// WORKS FOR MATH EQUATION
-// int main(void)
-// {
-//   int n = get_int("Start size: ");
-//   // int z = get_int("End size: ");
-//   int result = n + (n / 3) - (n / 4);
-//   printf("%i\n", result);
-// }
-
-// /////////////////////////////////
-
-// int main(void)
-// {
-//   int n;
-//   int z;
-//   n = get_int("Start size: ");
-//   z = get_int("End size: ");
-//   // y = get_int("Years: ");
-
-//   // int result = n + (n / 3) - (n / 4);
-  
-//   // int years = 0;
-
-//   // for (int result = 0; result < z; years++)
-//   // {
-    
-   
-//   //   printf("Years: %i\n", years);
-//   // }
-
-//   while
-
-// }
-
-
-// /////////////////////////////////
-
 int main(void)
 {
-    // TODO: Prompt for start size
+    // Prompt for start size
   	int start; 
   	do
   	{
@@ -48,8 +11,7 @@ int main(void)
   	} 
   	while (start < 9);
   
-
-    // TODO: Prompt for end size
+    // Prompt for end size
   	int end;
     do
     {
@@ -61,13 +23,13 @@ int main(void)
 	// Keep track of number of years
 	int years = 0;
 	
-	// TODO: Calculate number of years until we reach threshold
+	// Calculate number of years until we reach threshold
 	while (start < end)
 	{
 		start = start + (start / 3) - (start / 4);
 		years++;
 	}
 
-//     // TODO: Print number of years
+	// Print number of years
 	printf("Years: %i\n", years);
 }
